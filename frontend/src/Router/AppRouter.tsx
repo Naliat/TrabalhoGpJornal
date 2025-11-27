18a7b6e227aac14692817e273d2fc52f55a6c1bc
Rubens_Rabelo <rubensrabelo123@gmail.com>
2025-11-27 18:43:10 -0300

    feat(frontend): Adiciona componente de navbar

--- frontend/src/Router/AppRouter.tsx
+++ frontend/src/Router/AppRouter.tsx
@@ -1,14 +1,17 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Landing from "../pages/Landing/Landing";
+import ProtectedLayout from "../layout/ProtectedLayout";
 
 function AppRouter() {
     return (
         <>
-        <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Landing />} />
-            </Routes>
-        </BrowserRouter>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<ProtectedLayout />}>
+                        <Route path="/" element={<Landing />} />
+                    </Route>
+                </Routes>
+            </BrowserRouter>
         </>
     );
 }
